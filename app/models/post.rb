e23d392bfc893829fda74accba58b9f8c982d5d4
naeem asif <naeemgoraya@yahoo.com>
2013-10-28 17:29:42 +0500

    picture-uploaded-using-carrierwave

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ActiveRecord::Base
-  attr_accessible :text, :title
+  attr_accessible :text, :title, :picture
   validates_presence_of :title, :text
   validates_uniqueness_of :title
   has_many :comments , dependent: :destroy
+  mount_uploader :picture, PictureUploader
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131028092934) do
+ActiveRecord::Schema.define(:version => 20131028115715) do
 
   create_table "comments", :force => true do |t|
     t.string   "commenter"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(:version => 20131028092934) do
     t.text     "text"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "picture"
   end
 
 end
